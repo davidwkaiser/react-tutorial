e067936809caa98e854df0a7fd5e3e962ccf9b66
davidwkaiser <davidwkaiser@yahoo.com>
2018-06-28 22:47:33 -0500

    refactor to align with tutorial

--- src/index.js
+++ src/index.js
@@ -4,5 +4,16 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('myDiv'));
+
+
+
+// ReactDOM.render(<App />, document.getElementById('myDiv'));
+    ReactDOM.render(
+        <h1>Hello, world!</h1>,
+        document.getElementById('myDiv')
+    );
+
+
+
+
 registerServiceWorker();
